1607f8b859213f8d4f3c41b564510306e67e02f2
mcalvo84 <manu.calvo@gmail.com>
2019-03-12 23:00:12 +0100

    limpieza

--- src/app/app.routing.ts
+++
@@ -1,9 +0,0 @@
-// https://stackoverflow.com/questions/47353218/multiple-modules-and-routing-in-angular-5
-import {Routes, RouterModule} from '@angular/router';
-import {ModuleWithProviders} from '@angular/core';
-
-const routes: Routes = [
-    {path: 'submodule', loadChildren: 'app/submodule/submodule.module#SubModule'},
-];
-
-export const ModuleRouting: ModuleWithProviders = RouterModule.forRoot(routes);
