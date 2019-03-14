a740f530ffb89a60b0553468d4bd809f9256ca4a
mcalvo84 <manu.calvo@gmail.com>
2019-03-14 02:09:37 +0100

    delete all

--- Back/src_back/libs/boot.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    // Init
-    app.db.sequelize.sync().done(() => {
-        app.listen(app.get('port'), () => {
-            console.log('Server on port', app.get('port'));
-        })
-    });
-}
--- Back/src_back/libs/handlebars.js
+++
@@ -1,9 +0,0 @@
-const {format} = require('timeago.js');
-
-const helpers = {}
-
-helpers.timeago = (timestamp) => {
-    return format(timestamp);
-}
-
-module.exports = helpers;
